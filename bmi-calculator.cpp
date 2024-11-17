cbacc6976574a069d2f5c2bf876d3b26867a1c03
zamadshakil <146313727+zamadshakil@users.noreply.github.com>
2024-11-17 08:20:14 +0500

    Displaying value of BMI and added manipulators

--- bmi-calculator.cpp
+++ bmi-calculator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
@@ -6,7 +7,7 @@ int main(){
     double heightFeets, heightInches, heightMeters, weight;
     
     cout<<"Welcome to BMI Calculator!"<<endl;
-    
+
     cout<<"Enter your height in :"<<endl;
     
     cout << "Feets: ";
@@ -15,11 +16,13 @@ int main(){
     cout << "Inches: ";
     cin >> heightInches;
 
-    cout << "Enter your weight in pounds: ";
+    cout << "Enter your weight in kilograms: ";
     cin >> weight;
     
-
-
+    heightMeters = (heightFeets * 0.3048) + (heightInches * 0.0254);
+    
+    double bmi = weight / (heightMeters * heightMeters);
+    cout<<"BMI value is: "<<setprecision(2)<<bmi<<endl;
 
 
 
