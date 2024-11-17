08f07cda904f0fcd7233828a3dfd4763b8dfa9fe
zamadshakil <146313727+zamadshakil@users.noreply.github.com>
2024-11-17 08:13:36 +0500

    Upto Input Values

---
+++ bmi-calculator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    double heightFeets, heightInches, heightMeters, weight;
+    
+    cout<<"Welcome to BMI Calculator!"<<endl;
+    
+    cout<<"Enter your height in :"<<endl;
+    
+    cout << "Feets: ";
+    cin >> heightFeets;
+
+    cout << "Inches: ";
+    cin >> heightInches;
+
+    cout << "Enter your weight in pounds: ";
+    cin >> weight;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
